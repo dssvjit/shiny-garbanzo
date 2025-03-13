34bea301bf13a89d2fadee83b6c3c5767a4a3da9
SneakyBuzZ <kaushikx304@gmail.com>
2025-03-13 14:25:24 +0530

    securely redirected

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,10 +3,10 @@ export const GITHUB_CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENT_ID;
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 export const SERVER_API_URL = `${SERVER_URL}/api`;
 
-export const GITHUB_AUTH_REDIRECT_URL = `http://club.dssvjit.in/auth/callback/github`;
+export const GITHUB_AUTH_REDIRECT_URL = `https://club.dssvjit.in/auth/callback/github`;
 export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${GITHUB_AUTH_REDIRECT_URL}&scope=user:email,read:org`;
 
-export const GOOGLE_AUTH_REDIRECT_URL = `http://club.dssvjit.in/auth/callback/google`;
+export const GOOGLE_AUTH_REDIRECT_URL = `https://club.dssvjit.in/auth/callback/google`;
 export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_AUTH_REDIRECT_URL}&response_type=code&scope=email%20profile%20openid&access_type=offline&prompt=consent`;
 
 export const DSS_INSTAGRAM_URL = "https://www.instagram.com/dss_vjit/";
